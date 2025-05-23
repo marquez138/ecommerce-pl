3f9ef2a499d83bbbec7bdd5db364a3ca22db8338
marquez138 <somose138@gmail.com>
2025-05-22 20:21:26 -1000

    edit out stripe

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,18 +1,17 @@
+import { revalidateRedirects } from '@/hooks/revalidateRedirects'
+import { beforeSyncWithSearch } from '@/search/beforeSync'
+import { searchFields } from '@/search/fieldOverrides'
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { formBuilderPlugin } from '@payloadcms/plugin-form-builder'
 import { nestedDocsPlugin } from '@payloadcms/plugin-nested-docs'
 import { redirectsPlugin } from '@payloadcms/plugin-redirects'
-import { seoPlugin } from '@payloadcms/plugin-seo'
 import { searchPlugin } from '@payloadcms/plugin-search'
-import { Plugin } from 'payload'
-import { revalidateRedirects } from '@/hooks/revalidateRedirects'
+import { seoPlugin } from '@payloadcms/plugin-seo'
 import { GenerateTitle, GenerateURL } from '@payloadcms/plugin-seo/types'
 import { FixedToolbarFeature, HeadingFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
-import { searchFields } from '@/search/fieldOverrides'
-import { beforeSyncWithSearch } from '@/search/beforeSync'
-import { stripePlugin } from '@payloadcms/plugin-stripe'
+import { Plugin } from 'payload'
 
-import { paymentSucceeded } from '@/stripe/webhooks/paymentSucceeded'
+// import { paymentSucceeded } from '@/stripe/webhooks/paymentSucceeded'
 
 import { Page, Product } from '@/payload-types'
 import { getServerSideURL } from '@/utilities/getURL'
@@ -93,16 +92,16 @@ export const plugins: Plugin[] = [
       },
     },
   }),
-  stripePlugin({
-    isTestKey: Boolean(process.env.PAYLOAD_PUBLIC_STRIPE_IS_TEST_KEY),
-    logs: true,
-    rest: false,
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY || '',
-    stripeWebhooksEndpointSecret: process.env.STRIPE_WEBHOOKS_SIGNING_SECRET,
-    webhooks: {
-      'payment_intent.succeeded': paymentSucceeded,
-    },
-  }),
+  // stripePlugin({
+  //   isTestKey: Boolean(process.env.PAYLOAD_PUBLIC_STRIPE_IS_TEST_KEY),
+  //   logs: true,
+  //   rest: false,
+  //   stripeSecretKey: process.env.STRIPE_SECRET_KEY || '',
+  //   stripeWebhooksEndpointSecret: process.env.STRIPE_WEBHOOKS_SIGNING_SECRET,
+  //   webhooks: {
+  //     'payment_intent.succeeded': paymentSucceeded,
+  //   },
+  // }),
 
   payloadCloudPlugin(),
 ]
