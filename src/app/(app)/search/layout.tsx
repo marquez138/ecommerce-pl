71028bc60b04b6120137028cfbe95d1b8f66d522
marquez138 <somose138@gmail.com>
2025-05-22 20:44:13 -1000

    edit search

--- src/app/(app)/search/layout.tsx
+++ src/app/(app)/search/layout.tsx
@@ -1,14 +1,17 @@
+import { Search, SearchSkeleton } from '@/components/Header/Search'
 import { Categories } from '@/components/layout/search/Categories'
 import { FilterList } from '@/components/layout/search/filter'
 import { sorting } from '@/lib/constants'
-import { Search } from '@/components/Header/Search'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
   return (
     <React.Fragment>
       <div className="container flex flex-col gap-8 my-16 pb-4 ">
-        <Search />
+        <Suspense fallback={<SearchSkeleton />}>
+
+          <Search />
+        </Suspense>
         <div className="w-full flex-none">
           <Categories />
           <FilterList list={sorting} title="Sort by" />
